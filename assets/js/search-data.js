4556e3eba97f0fe2c0b4fe36207e99692f2945c3
hfahrudin <fahrudinhasby12@gmail.com>
2025-08-18 01:21:15 +0000

    Deploying to gh-pages from @ hfahrudin/hfahrudin.github.io@985cc7642180a9f46c59372f5eaa46d3d25eec45 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -114,8 +114,8 @@ ninja.data = [{
           description: "Improve Online VR Experiences with Enhancement of Head Motion Prediction",
           section: "Projects",handler: () => {
               window.location.href = "/projects/12/";
-            },},{id: "projects-facex",
-          title: 'facex',
+            },},{id: "projects-orkes",
+          title: 'Orkes',
           description: "A lightweight orchestration framework for LLM agent",
           section: "Projects",handler: () => {
               window.location.href = "/projects/13/";
